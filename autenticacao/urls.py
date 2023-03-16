38d3591d3be1059e11b5262fb3517d644c6b9791
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-03-16 10:01:20 -0300

    validações

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("login/", views.login, name='login'),
+    path("login/", views.logar, name='logar'),
     path("cadastro/", views.cadastro, name='cadastro'),
     path("logout/", views.logout, name='logout'),
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.contrib.auth.models import User
 
 # Create your views here.
-def login(request):
+def logar(request):
     return HttpResponse('Area Login')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Não pode conter espaços em branco')
+            return redirect('/auth/cadastro')
+
+        if len(senha) < 5:
+            messages.add_message(request, constants.ERROR, 'Sua senha tem que conter mais de 5 caracteres')
+            return redirect('/auth/cadastro')
+        
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe')
+            return redirect('/auth/cadastro')
+        
+        try:
+            user = User(username=username, email=email, password=senha)
+
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuário salvo com sucesso')
+            return redirect('auth/cadastro')
+        
+        except:
+            return redirect('/auth/cadastro')
+
 
 def logout(request):
     return HttpResponse('Area de logout')
