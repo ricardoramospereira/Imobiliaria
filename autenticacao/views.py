6c3788aea0f96853abeb52f162bb3fb1033f4d9f
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-03-21 14:45:02 -0300

    filter

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,25 +2,33 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
-def logar(request):
-    return HttpResponse('Area Login')
 
 def cadastro(request):
-    if request.method == 'GET':
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return request('/')
         return render(request, 'cadastro.html')
-    elif request.method == 'POST':
+    elif request.method == "POST":
         username = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
 
-        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
-            messages.add_message(request, constants.ERROR, 'Não pode conter espaços em branco')
-            return redirect('/auth/cadastro')
-
-        if len(senha) < 5:
-            messages.add_message(request, constants.ERROR, 'Sua senha tem que conter mais de 5 caracteres')
+        if len(username.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Campo username não pode ficar em branco' )
+            return redirect('cadastro')
+        
+        if len(email.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Campo email não pode ficar em branco')
+            return redirect('cadastro')
+        
+        if len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Campo senha não pode ficar vazio')
+            return redirect('cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'Sua senha deve conter 6 ou mais caracteres')
             return redirect('/auth/cadastro')
         
         user = User.objects.filter(username=username)
@@ -30,15 +38,35 @@ def cadastro(request):
             return redirect('/auth/cadastro')
         
         try:
-            user = User(username=username, email=email, password=senha)
+            user = User.objects.create_user(username=username, email=email, password=senha)
 
             user.save()
-            messages.add_message(request, constants.SUCCESS, 'Usuário salvo com sucesso')
-            return redirect('auth/cadastro')
-        
-        except:
-            return redirect('/auth/cadastro')
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso')
+            return redirect('/auth/login')
 
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('auth/cadastro')
+           
+
+def login(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return request('/')
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos')
+            return redirect('/auth/login')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
 
 def logout(request):
-    return HttpResponse('Area de logout')
+    auth.logout(request)
+    return redirect('/auth/login')
--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "autenticacao.apps.AutenticacaoConfig",
+    "plataforma.apps.PlataformaConfig",
 ]
 
 MIDDLEWARE = [
